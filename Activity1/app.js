c8f7bc51ae6c8327dd47a31a927252a1bed9a26c
tacciiyy <caballlerotracy13@gmail.com>
2024-03-19 17:32:06 +0800

    my first commit

---
+++ Activity1/app.js
@@ -0,0 +1,19 @@
+import { myimage } from "./img.js"
+import { myFunction } from "./info.js"
+
+let myObject = {
+    title1 : "The 3 Puppies",
+    paragraph1 : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, sed inventore! Quidem blanditiis assumenda cum placeat harum nihil praesentium nemo sit numquam quo quos culpa rem, incidunt consequatur magni ab!",
+    myimg: "3pups.jpg",
+    button1: "contact us",
+    button2: "about us",
+    title: "Title 2",
+    paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, sed inventore! Quidem blanditiis assumenda cum placeat harum nihil praesentium nemo sit numquam quo quos culpa rem, incidunt consequatur magni ab!",
+    button3: "contact us",
+    button4: "about us"
+} 
+
+const {title1, paragraph1, myimg, button1, button2, title, paragraph, button3, button4} = myObject
+let container = document.getElementById("container")
+container.append(myFunction(title1,title, paragraph1,paragraph, button1,button2, button3, button4))
+container.append(myimage(myimg))
